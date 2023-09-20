ff9ee75f1758971c28ade99d5d7a8645339d936e
ryunroo <dlgustn7824@naver.com>
2023-09-20 15:29:20 +0900

    Feat: "슈팅 매니저 멀티 샷 설정"

--- Unity_Algorithm/Assets/Scripts/Util/SGUtil.cs
+++ Unity_Algorithm/Assets/Scripts/Util/SGUtil.cs
@@ -15,8 +15,8 @@ public static class SGUtil
     public static readonly Quaternion QUATERNION_IDENTITY = Quaternion.identity;
     public enum AXIS
     {
-        X_AXIS_Y,
-        X_AXIS_Z,
+        X_AND_Y,
+        X_AND_Z,
     }
 
     public enum TIME
@@ -26,48 +26,51 @@ public static class SGUtil
         FIXED_DELTA_TIME,
     }
 
+
     public static float GetAngleFromTwoPosition(Transform fromTrans, Transform toTrans, AXIS axisMove)
     {
         switch (axisMove)
         {
-            case AXIS.X_AXIS_Y:
-                return 0;//GetZangleFromTwoPosition(fromTrans,toTrans);
-            case AXIS.X_AXIS_Z:
-                return 0;//GetYangleFromTwoPosition(fromTrans,toTrans);
+            case AXIS.X_AND_Y:
+                return GetZangleFromTwoPosition(fromTrans, toTrans);
+            case AXIS.X_AND_Z:
+                return GetYangleFromTwoPosition(fromTrans, toTrans);
             default:
-                return 0;
+                return 0f;
         }
+
+
     }
 
     private static float GetZangleFromTwoPosition(Transform fromTrans, Transform toTrans)
     {
-        if (fromTrans == null || toTrans == null)            //값이 들어왔는지 확인
+        if (fromTrans == null || toTrans == null)
         {
             return 0f;
         }
-
-        float xDistance = toTrans.position.x - fromTrans.position.x;                    //X축 거리
-        float yDistance = toTrans.position.y - fromTrans.position.y;                    //Y축 거리
-        float angle = (Mathf.Atan2(yDistance, xDistance) * Mathf.Rad2Deg) - 90.0f;      //두 Transform의 각도
+        float xDistance = toTrans.position.x - fromTrans.position.x;
+        float yDistance = toTrans.position.y - fromTrans.position.y;
+        float angle = (Mathf.Atan2(yDistance, xDistance) * Mathf.Rad2Deg) - 90f;
         angle = GetNormalizedAngle(angle);
+
         return angle;
     }
 
     private static float GetYangleFromTwoPosition(Transform fromTrans, Transform toTrans)
     {
-        if (fromTrans == null || toTrans == null)            //값이 들어왔는지 확인
+        if (fromTrans == null || toTrans == null)
         {
             return 0f;
         }
-
-        float xDistance = toTrans.position.x - fromTrans.position.x;                    //X축 거리
-        float zDistance = toTrans.position.z - fromTrans.position.z;                    //Y축 거리
-        float angle = (Mathf.Atan2(zDistance, xDistance) * Mathf.Rad2Deg) - 90.0f;      //두 Transform의 각도
+        float xDistance = toTrans.position.x - fromTrans.position.x;
+        float zDistance = toTrans.position.z - fromTrans.position.z;
+        float angle = (Mathf.Atan2(zDistance, xDistance) * Mathf.Rad2Deg) - 90f;
         angle = GetNormalizedAngle(angle);
+
         return angle;
     }
 
-    public static float GetNormalizedAngle(float angle)                 //계산한 각도 값을 0 ~ 360 사이에 보정 
+    public static float GetNormalizedAngle(float angle)
     {
         while (angle < 0f)
         {
@@ -81,3 +84,6 @@ public static class SGUtil
     }
 
 }
+
+
+
